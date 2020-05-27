37e40113d3c7efa6bba314913b9317a5c3b62081
Luchtmeyer <maik.luchtmeyer@axelspringer.com>
2020-05-27 16:11:04 +0200

    Use new release

--- index.js
+++ index.js
@@ -5,28 +5,26 @@ const path = require("path");
 
 async function run() {
     try {
-        let content;
         const filePath = path.join(__dirname, "template.md");
         fs.readFile(filePath, "utf8", (err, data) => {
             if (err) {
-                core.setFailed("Could not read template");
+                core.setFailed(err);
             }
-            content = data;
-        });
-        const github_token = core.getInput("GITHUB_TOKEN");
+            const github_token = core.getInput("GITHUB_TOKEN");
 
-        const context = github.context;
-        if (context.payload.pull_request == null) {
-            core.setFailed("No pull request found.");
-            return;
-        }
-        const pull_request_number = context.payload.pull_request.number;
+            const context = github.context;
+            if (context.payload.pull_request == null) {
+                core.setFailed("No pull request found.");
+                return;
+            }
+            const pull_request_number = context.payload.pull_request.number;
 
-        const octokit = new github.GitHub(github_token);
-        const new_comment = octokit.issues.createComment({
-            ...context.repo,
-            issue_number: pull_request_number,
-            body: content,
+            const octokit = new github.GitHub(github_token);
+            const new_comment = octokit.issues.createComment({
+                ...context.repo,
+                issue_number: pull_request_number,
+                body: data,
+            });
         });
     } catch (error) {
         core.setFailed(error.message);
